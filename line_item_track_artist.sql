ddbf9494bf52c50ec28a889c7e3a2f3cb51465a8
Zach Nicholson <raiste.doctow@gmail.com>
2020-10-03 16:46:22 -0500

    updated query to include album artist, not track composer

--- line_item_track_artist.sql
+++ line_item_track_artist.sql
@@ -1,8 +1,10 @@
 --Provide a query that includes the purchased track name AND artist name with each invoice line item.
 
 SELECT
-    t.Name,
-    t.Composer,
+    t.Name AS Track,
+    a.Name AS Artist,
     il.*
 FROM Track t
-JOIN InvoiceLine il ON il.TrackId = t.TrackId;
+JOIN InvoiceLine il ON il.TrackId = t.TrackId
+JOIN Album ON Album.AlbumId = t.AlbumId
+JOIN Artist a ON a.ArtistId = Album.ArtistId;
