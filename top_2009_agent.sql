1da577c46aacb14c72d5d974c43098c8b0e90297
Zach Nicholson <raiste.doctow@gmail.com>
2020-10-05 21:33:11 -0500

    steve is the top agent, of course

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,15 +1,8 @@
 --Which sales agent made the most in sales in 2009?
 
-SELECT 
-    ROUND(SUM(i.Total), 2) AS TotalSales,
-    e.FirstName || ' ' || e.LastName AS SalesAgent
-FROM Employee e
-JOIN Customer c ON c.SupportRepId = e.EmployeeId
-JOIN Invoice i ON i.CustomerId = c.CustomerId AND i.InvoiceDate LIKE '2009%'
-GROUP BY SalesAgent;
-
 SELECT
-    s.SalesAgent AS SalesAgent
+    SalesAgent,
+    MAX(TotalSales) as Total
 FROM (
     SELECT 
         ROUND(SUM(i.Total), 2) AS TotalSales,
@@ -19,8 +12,4 @@ FROM (
     JOIN Customer c ON c.SupportRepId = e.EmployeeId
     JOIN Invoice i ON i.CustomerId = c.CustomerId AND i.InvoiceDate LIKE '2009%'
     GROUP BY SalesAgent
-) s
-WHERE s.TotalSales = 
-    (SELECT MAX(s.TotalSales) AS TopSales
-     FROM Invoice i1
-     WHERE i1.InvoiceId = s.InvoiceId);
+);
