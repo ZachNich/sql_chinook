23ee57f8564f40ce67a5706f98168b861a33c655
Zach Nicholson <raiste.doctow@gmail.com>
2020-10-09 17:09:37 -0500

    queried topsales country, just countryname

--- top_country.sql
+++ top_country.sql
@@ -1,8 +1,25 @@
 --Which country's customers spent the most?
 
 SELECT
+    Country
+FROM (
+    SELECT
+        ROUND(SUM(i.Total), 2) AS TotalSales,
+        i.BillingCountry AS Country
+    FROM Invoice i
+    GROUP BY Country
+)
+WHERE TotalSales = (SELECT MAX(TotalSales) FROM (
+    SELECT
+        ROUND(SUM(i.Total), 2) AS TotalSales,
+        i.BillingCountry AS Country
+    FROM Invoice i
+    GROUP BY Country
+));
+
+SELECT
     Country,
-    MAX(TotalSales) AS TopSales
+    MAX(TotalSales)
 FROM (
     SELECT
         ROUND(SUM(i.Total), 2) AS TotalSales,
